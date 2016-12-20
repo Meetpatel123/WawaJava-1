e8d117901150a8e07235dac2dc559df20583cd7a
Konrad Grzanek <kongra@gmail.com>
2016-12-20 13:58:52 +0100

    Heron.java added

---
+++ src/Heron.java
@@ -0,0 +1,40 @@
+public class Heron {
+
+	static double sqrt(double y, double x) {
+		if(isGoodEnough(y, x))
+			return y;
+		else
+			return sqrt(f(y, x), x);
+	}
+
+	static double f(double y, double x) {
+		return avg(y, x / y);
+	}
+
+	static double avg(double x, double y) {
+		return (x + y) / 2;
+	}
+
+	static boolean isGoodEnough(double y, double x) {
+		return abs(square(y) - x) < e;
+	}
+
+	static double abs(double x) {
+		if(x < 0)
+			return -x;
+		else
+			return x;
+	}
+
+	static double square(double x) {
+		return x * x;
+	}
+
+	static final double e = 0.00001;
+
+	public static void main(String[] args) {
+		double y1 = sqrt(1, 4);
+		System.out.println(y1);
+	}
+
+}
