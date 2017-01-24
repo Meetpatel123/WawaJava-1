86e7481e0c308c937736fd94262316732ea8379b
Konrad Grzanek <kongra@gmail.com>
2017-01-24 13:57:49 +0100

    Car added.

---
+++ src/oo/Car.java
@@ -0,0 +1,38 @@
+package oo;
+
+class Car {
+
+  double acc;
+
+  double maxSpeed;
+
+  double speed;
+
+  public Car(double acc, double maxSpeedkmPh) {
+    this.acc = acc;
+    this.maxSpeed = maxSpeedkmPh * 1000.0 / 3600.0;
+  }
+
+  void start() {
+    long time = 0;
+    for(;;) {
+      time += 1;
+      double newSpeed = this.speed + this.acc;
+      if(newSpeed <= this.maxSpeed) {
+        this.speed = newSpeed;
+      }
+      System.out.println("My current speed is " + this.speed);
+
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    Car c = new Car(2, 230);
+    c.start();
+  }
+}
