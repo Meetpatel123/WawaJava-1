9a4929d0113e275e877d4953ecfd9bd081a19f9f
Konrad Grzanek <kongra@gmail.com>
2017-03-14 12:04:10 +0100

    HTML/JS example added.

---
+++ html/program.js
@@ -0,0 +1,21 @@
+var d = {
+  "dom": "house",
+  "brama": "gate",
+  "ogród": "garden",
+  "lekcja": "lesson"
+};
+
+function factorial(n) {
+  if (n == 0) return 1;
+  else return n * factorial(n-1);
+}
+
+function onActionPerformed() {
+  var n = parseInt(document.getElementById("n").value);
+  if (isNa(n)) {
+    document.getElementById("result").innerHTML = "";
+  }
+  else {
+    document.getElementById("result").innerHTML = n;
+  }
+}
