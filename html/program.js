961e0a8c532842651960d67bb8a3ba99c9560573
Konrad Grzanek <kongra@gmail.com>
2017-03-21 10:10:58 +0100

    -

--- html/program.js
+++ html/program.js
@@ -17,11 +17,49 @@ function factorial(n) {
 
 var document;
 
+function fib(n) {
+    if (n === 0 || n === 1) {
+        return n;
+    } else {
+        return fib(n - 1) + fib(n - 2);
+    }
+}
+
+function withMsecs(expr) {
+    var t1 = performance.now();
+    var value = expr();
+    var t2 = performance.now();
+    return {value: value, msecs: (t2 - t1)};
+}
+
+function logging(msg, expr) {
+    console.log("STARTING " + msg + " ...");
+    var value = expr();
+    console.log("... DONE");
+    return value;
+}
+
+function loggingWithMsecs(msg, expr) {
+    return logging(msg, function() {
+        return withMsecs(expr);
+    });
+}
+
 function onActionPerformed() {
     var n = parseInt(document.getElementById("n").value);
     if (isNaN(n)) {
         document.getElementById("result").innerHTML = "";
     } else {
-        document.getElementById("result").innerHTML = factorial(n);
+        var result = loggingWithMsecs("fib(n)", function() {
+            return fib(n);
+        });
+        document.getElementById("result").innerHTML = "" + result.value +
+            " in " + result.msecs + " msecs";
+
+        // var t1 = performance.now();
+        // var v  = fib(n);
+        // var t2 = performance.now();
+        // document.getElementById("result").innerHTML = "" + v + " in " +
+        //     (t2 - t1) + " msecs";
     }
 }
